07895c463bff14b80050cdae34910d69f686f514
judithgispert <jugispert@gmail.com>
2024-01-19 11:53:31 +0100

    Correction type

--- src/main/java/cat/itacademy/barcelonactiva/Gispert/Judith/s04/t02/n02/Repository/IFruitRepo.java
+++ src/main/java/cat/itacademy/barcelonactiva/Gispert/Judith/s04/t02/n02/Repository/IFruitRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IFruitRepo extends JpaRepository<Fruit, Float> { }
+public interface IFruitRepo extends JpaRepository<Fruit, Integer> { }
 
